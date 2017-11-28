2cc64f87131f801744d22c75bfa90c8541182dab
Fabien CHEVREY <fchevrey@e3r10p19.42.fr>
2017-11-28 19:22:09 +0100

    struct

--- src/fillit.h
+++ src/fillit.h
@@ -6,20 +6,20 @@
 /*   By: overetou <overetou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 17:59:35 by overetou          #+#    #+#             */
-/*   Updated: 2017/11/28 17:59:55 by overetou         ###   ########.fr       */
+/*   Updated: 2017/11/28 19:21:49 by fchevrey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
 
-typedef struct	s_trio 
+typedef struct	s_trio
 {
 	char			*l1;
 	char			*l2;
 	char			*l3;
 	char			*l4;
-	stuct s_trio	*next;
+	struct s_trio	*next;
 	char			letr;
 }				t_trio;
 
