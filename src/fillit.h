60c396db8be18a2f5a716c00fd390c56df313111
Fabien CHEVREY <fchevrey@e3r13p3.42.fr>
2017-11-29 20:07:20 +0100

    mapnew

--- src/fillit.h
+++ src/fillit.h
@@ -6,7 +6,7 @@
 /*   By: overetou <overetou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 17:59:35 by overetou          #+#    #+#             */
-/*   Updated: 2017/11/29 16:36:42 by fchevrey         ###   ########.fr       */
+/*   Updated: 2017/11/29 20:00:16 by fchevrey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,5 @@ void	ft_tadd(t_trio **atrio, t_trio *new);
 t_trio	*ft_tnew(char *str, char ltr);
 char	*ft_flcpy(char *flname);
 short	ft_check(char *cpy);
-
+char	**ft_mapnew(int size);
 #endif
