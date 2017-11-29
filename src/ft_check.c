d1a6a02782929b23952e2f8c30c19be1af8fecfe
overetou <overetou@student.42.fr>
2017-11-29 17:36:24 +0100

    alpha de ft_check

--- src/ft_check.c
+++ src/ft_check.c
@@ -21,23 +21,30 @@ short	ft_check_argc(int argc)
 
 short	ft_countbs(char *cpy)
 {
-	unsigned int	pts;
-	unsigned int	htgs;
+	unsigned int	pt;
+	unsigned int	htg;
 	unsigned int	endl;
 
-	while (cpy++)
+	while (cpy)
 	{
-		if (*cpy != '.' && *cpy != '#' &&)
-		if (*cpy == '.')
-			pts++;
-		if (*cpy == '#')
-			htgs++;
-		if (*cpy == '\n')
+		pt = 0;
+		htg = 0;
+		endl = 0;
+		while (endl % 5 && *cpy)
 		{
-			endl++;
-			if ((pts + htgs) % 5)
+			if (*cpy == '.')
+				pt++;
+			if (*cpy == '#')
+				htg++;
+			if (*cpy == '\n')
+				endl++;
+			cpy++;
 		}
+		if ((pt + htg) % 16)
+			return (0);
+		cpy++;
 	}
+	return (1);
 }
 
 short	ft_check(int argc, char *cpy)
